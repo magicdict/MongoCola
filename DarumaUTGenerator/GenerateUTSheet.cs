e54c57b6b9a5d7e78193a30ccdee4f21b570e19e
MagicHu <huweimin@cn.ibm.com>
2013-10-30 09:45:39 +0800

    将式样书做成独立成一个项目，原来的代码停止维护

---
+++ DarumaUTGenerator/GenerateUTSheet.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DarumaUTGenerator
+{
+    public static class GenerateUTSheet
+    {
+        const int xlToRight = -4161;
+        const int xlFormatFromLeftOrAbove = 0;
+        const int xlLeft = -4131;
+        const int xlNone = -4142;
+        const int xlContinuous = 1;
+        const int xlEdgeLeft = 7;
+        const int xlEdgeTop = 8;
+        const int xlEdgeBottom = 9;
+        const int xlEdgeRight = 10;
+        const int xlInsideVertical = 11;
+        const int xlInsideHorizontal = 12;
+        static int MaxLv;
+        public static void GenerateUT(IDL2PgmStruct pgm, String UTFilename)
+        {
+            dynamic excelObj = Microsoft.VisualBasic.Interaction.CreateObject("Excel.Application");
+            int RowCount;
+            excelObj.Visible = true;
+            dynamic workbook = excelObj.Workbooks.Open(UTFilename);
+            dynamic ActiveSheet = workbook.Sheets(3);
+            ActiveSheet.Select();
+            ///L1实际上是用来标明分歧号的，所以必须多加一列
+            MaxLv = pgm.MaxNestLv + 1;
+            if (MaxLv > 5)
+            {
+                ActiveSheet.Cells(4, 1).Select();
+                excelObj.Selection.UnMerge();
+                ActiveSheet.Columns(5).Select();
+                for (int i = 0; i < MaxLv - 5; i++)
+                {
+                    excelObj.Selection.Insert(Shift: xlToRight, CopyOrigin: xlFormatFromLeftOrAbove);
+                }
+                excelObj.Range(ActiveSheet.Cells(4, 1), ActiveSheet.Cells(4, MaxLv)).Select();
+                excelObj.Selection.Merge();
+                for (int i = 1; i < MaxLv + 1; i++)
+                {
+                    ActiveSheet.Cells(5, i).Value = "L" + i;
+                }
+            }
+            else
+            {
+                MaxLv = 5;
+            }
+            RowCount = 6;
+
+            int BranchNo;
+            foreach (var section in pgm.SectionList)
+            {
+                CreateSectionHeader(RowCount, section.SectionName, excelObj, ActiveSheet);
+                RowCount = RowCount + 1;
+                BranchNo = 1;
+                foreach (var Syntax in section.SyntaxList)
+                {
+                    ActiveSheet.Cells(RowCount, 1).Value = BranchNo;
+                    int StartRow = RowCount;
+                    int EndRow;
+                    IDL2PgmStruct.Syntax CaseSyntax = new IDL2PgmStruct.Syntax();
+                    String KeyWord;
+                    int WhenNo = 0;
+                    for (int i = 0; i < Syntax.Count; i++)
+                    {
+                        var SyntaxItem = Syntax[i];
+                        KeyWord = SyntaxItem.SyntaxType;
+                        if (SyntaxItem.SyntaxType == "WHEN")
+                        {
+                            WhenNo = 1;
+                            //寻找最近的父CASE语句
+                            for (int j = i - 1; j >= 0; j--)
+                            {
+                                if (Syntax[j].SyntaxType == "CASE")
+                                {
+                                    CaseSyntax = Syntax[j];
+                                    break;
+                                }
+                                if (Syntax[j].SyntaxType == "WHEN")
+                                {
+                                    WhenNo++;
+                                }
+                            }
+                        }
+                        if (IDL2PgmStruct.isStartSyntax(SyntaxItem) || IDL2PgmStruct.isMidSyntax(SyntaxItem))
+                        {
+                            //LineNo
+                            ActiveSheet.Cells(RowCount, MaxLv + 1).Value = SyntaxItem.LineNo;
+                            //测试内容
+                            String TestString = String.Empty;
+                            if (SyntaxItem.Cond != null)
+                            {
+                                foreach (var item in SyntaxItem.Cond.TestItemLst)
+                                {
+                                    TestString += "「" + item + "」、";
+                                }
+                            }
+                            TestString = TestString.TrimEnd("、".ToCharArray());
+                            switch (SyntaxItem.SyntaxType)
+                            {
+                                case "IF":
+                                    TestString += "の分岐判定処理";
+                                    ActiveSheet.Cells(RowCount, MaxLv + 4).Value = TestString;
+                                    break;
+                                case "ELSE":
+                                    ActiveSheet.Cells(RowCount, MaxLv + 5).Value = "上記以外";
+                                    break;
+                                case "WHILE":
+                                case "REPEAT":
+                                case "FOR":
+                                    TestString += "終了条件まで繰り返す";
+                                    ActiveSheet.Cells(RowCount, MaxLv + 4).Value = TestString;
+                                    break;
+                                case "LOOP":
+                                    ActiveSheet.Cells(RowCount, MaxLv + 4).Value = "ループをBREAKまで繰り返す";
+                                    break;
+                                case "WHEN":
+                                    if (CaseSyntax.ExtendInfo == IDL2PgmStruct.TrueFlg)
+                                    {
+                                        ActiveSheet.Cells(RowCount, MaxLv + 4).Value = "判定" + WhenNo;
+                                        ActiveSheet.Cells(RowCount, MaxLv + 5).Value = SyntaxItem.ExtendInfo;
+                                    }
+                                    else {
+                                        if (WhenNo == 1) {
+                                            ActiveSheet.Cells(RowCount, MaxLv + 4).Value = CaseSyntax.ExtendInfo;
+                                        }
+                                        ActiveSheet.Cells(RowCount, MaxLv + 5).Value = SyntaxItem.ExtendInfo;
+                                    }
+                                    break;
+                                default:
+                                    break;
+                            }
+                            //分歧条件
+                            if (SyntaxItem.Cond != null)
+                            {
+                                ActiveSheet.Cells(RowCount, MaxLv + 5).Value = SyntaxItem.Cond.OrgCondition;
+                            }
+                            //迁移情报
+                            ActiveSheet.Cells(RowCount, MaxLv + 6).Value = GetJump(SyntaxItem.JumpInfo);
+                            //KeyWord
+                            switch (KeyWord)
+                            {
+                                case "IF":
+                                    ActiveSheet.Cells(RowCount, MaxLv + 2).Value = KeyWord;
+                                    ActiveSheet.Cells(RowCount, MaxLv + 3).Value = "THEN";
+                                    break;
+                                case "CASE":
+                                    ActiveSheet.Cells(RowCount, MaxLv + 2).Value = KeyWord;
+                                    RowCount--;
+                                    break;
+                                case "ELSE":
+                                case "WHEN":
+                                    ActiveSheet.Cells(RowCount, MaxLv + 3).Value = KeyWord;
+                                    break;
+                                default:
+                                    ActiveSheet.Cells(RowCount, MaxLv + 2).Value = KeyWord;
+                                    break;
+                            }
+                            //Nest Lv
+                            if (KeyWord == "ELSE")
+                            {
+                                ActiveSheet.Cells(RowCount, SyntaxItem.NestLv + 1).Value = "2";
+                            }
+                            else
+                            {
+                                if (KeyWord == "WHEN")
+                                {
+                                    ActiveSheet.Cells(RowCount, SyntaxItem.NestLv + 1).Value = WhenNo;
+                                }
+                                else
+                                {
+                                    ActiveSheet.Cells(RowCount, SyntaxItem.NestLv + 1).Value = "1";
+                                }
+                            }
+                            RowCount = RowCount + 1;
+                        }
+                    }
+                    EndRow = RowCount - 1;
+                    ActiveSheet.Range(ActiveSheet.Cells(StartRow, 1), ActiveSheet.Cells(EndRow, 1)).Select();
+                    excelObj.Selection.Borders(xlEdgeLeft).LineStyle = xlContinuous;
+                    excelObj.Selection.Borders(xlEdgeTop).LineStyle = xlContinuous;
+                    excelObj.Selection.Borders(xlEdgeRight).LineStyle = xlContinuous;
+                    excelObj.Selection.Borders(xlEdgeBottom).LineStyle = xlContinuous;
+                    DrawNestLine(StartRow, EndRow, excelObj, ActiveSheet);
+                    ActiveSheet.Range(ActiveSheet.Cells(StartRow, MaxLv + 1), ActiveSheet.Cells(EndRow, MaxLv + 6)).Select();
+                    excelObj.Selection.Borders(xlEdgeLeft).LineStyle = xlContinuous;
+                    excelObj.Selection.Borders(xlEdgeTop).LineStyle = xlContinuous;
+                    excelObj.Selection.Borders(xlEdgeRight).LineStyle = xlContinuous;
+                    excelObj.Selection.Borders(xlEdgeBottom).LineStyle = xlContinuous;
+                    excelObj.Selection.Borders(xlInsideVertical).LineStyle = xlContinuous;
+                    excelObj.Selection.Borders(xlInsideHorizontal).LineStyle = xlContinuous;
+
+                    BranchNo++;
+                }
+            }
+            ///印刷设定
+            RowCount--;
+            ActiveSheet.PageSetup.PrintArea = "$A$1:$R$" + RowCount;
+        }
+
+        private static dynamic GetJump(List<IDL2PgmStruct.Syntax> list)
+        {
+            String strJumpInfo = String.Empty;
+            if (list.Count == 0)
+            {
+                strJumpInfo = "次の処理へ";
+            }
+            else
+            {
+                foreach (var JumpItem in list)
+                {
+                    if (JumpItem.SyntaxType == "ASSIGN")
+                    {
+                        strJumpInfo += JumpItem.ExtendInfo + "#";
+                    }
+                    else
+                    {
+                        strJumpInfo += "「" + JumpItem.ExtendInfo + "」に移る#";
+                    }
+                }
+                strJumpInfo = strJumpInfo.TrimEnd("#".ToCharArray()).Replace("#", System.Environment.NewLine);
+            }
+            return strJumpInfo;
+        }
+        /// <summary>
+        /// 画Nest构造
+        /// </summary>
+        /// <param name="StartRow"></param>
+        /// <param name="EndRow"></param>
+        /// <param name="excelObj"></param>
+        /// <param name="ActiveSheet"></param>
+        private static void DrawNestLine(int StartRow, int EndRow, dynamic excelObj, dynamic ActiveSheet)
+        {
+            for (int i = StartRow; i < EndRow + 1; i++)
+            {
+                for (int j = 2; j < MaxLv + 1; j++)
+                {
+                    if (ActiveSheet.Cells(i, j).Text != "")
+                    {
+                        //向左和向下的线
+                        ActiveSheet.Range(ActiveSheet.Cells(i, j), ActiveSheet.Cells(EndRow, MaxLv)).Select();
+                        excelObj.Selection.Borders(xlEdgeLeft).LineStyle = xlContinuous;
+                        excelObj.Selection.Borders(xlEdgeTop).LineStyle = xlContinuous;
+                        excelObj.Selection.Borders(xlEdgeRight).LineStyle = xlContinuous;
+                        excelObj.Selection.Borders(xlEdgeBottom).LineStyle = xlContinuous;
+                    }
+                }
+            }
+            for (int i = StartRow; i < EndRow + 1; i++)
+            {
+                for (int j = 2; j < MaxLv + 1; j++)
+                {
+                    if (ActiveSheet.Cells(i, j).Text != "")
+                    {
+                        //去除右边的多余线
+                        ActiveSheet.Range(ActiveSheet.Cells(i, j), ActiveSheet.Cells(i, MaxLv)).Select();
+                        excelObj.Selection.Borders(xlInsideVertical).LineStyle = xlNone;
+                    }
+
+                }
+            }
+        }
+        /// <summary>
+        /// 画Section块
+        /// </summary>
+        /// <param name="RowCount"></param>
+        /// <param name="SectionName"></param>
+        private static void CreateSectionHeader(int RowCount, string SectionName, dynamic excelObj, dynamic ActiveSheet)
+        {
+            ActiveSheet.Range(ActiveSheet.Cells(RowCount, 1), ActiveSheet.Cells(RowCount, MaxLv + 6)).Select();
+            excelObj.Selection.Merge();
+            excelObj.Selection.HorizontalAlignment = xlLeft;
+            excelObj.Selection.Interior.Color = 6750207;
+            excelObj.Selection.Borders(xlEdgeLeft).LineStyle = xlContinuous;
+            excelObj.Selection.Borders(xlEdgeTop).LineStyle = xlContinuous;
+            excelObj.Selection.Borders(xlEdgeRight).LineStyle = xlContinuous;
+            excelObj.Selection.Borders(xlEdgeBottom).LineStyle = xlContinuous;
+            ActiveSheet.Cells(RowCount, 1).Value = SectionName;
+
+        }
+    }
+}
---
+++ DarumaUTGenerator/clsCondition.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DarumaUTGenerator
+{
+    public class clsCondition
+    {
+        const String CAnd = " AND ";
+        const String COr = " OR ";
+        const String CBig = " > ";
+        const String CBigEqual = " >= ";
+        const String CSmall = " < ";
+        const String CSmallEqual = " <= ";
+        const String CEqual = " = ";
+        const String CNotEqual = " ^= ";
+        String[] MathOplst = new string[] { CBig, CBigEqual, CSmall, CSmallEqual, CEqual, CNotEqual };
+        String[] LogicOplst = new string[] { CAnd, COr };
+        public List<String> TopConditionLst = new List<string>();
+        public List<String> TestItemLst = new List<string>();
+        public String OrgCondition = String.Empty;
+        /// <summary>
+        /// 构造器
+        /// </summary>
+        /// <param name="strCondition"></param>
+        public clsCondition(String strCondition)
+        {
+            OrgCondition = strCondition;
+            //"(" + 1
+            //")" - 1
+            int nestLV = 1;
+            string subCondition = String.Empty;
+            Boolean HasAdded = false;
+            Boolean IsIndexOn = false;
+            for (int i = 0; i < strCondition.Length; i++)
+            {
+                HasAdded = false;
+                if (strCondition.Substring(i, 1) == "(")
+                {
+                    if (nestLV == 1 && (!String.IsNullOrEmpty(subCondition)))
+                    {
+                        IsIndexOn = true;
+                    }
+                    else
+                    {
+                        if (nestLV == 1)
+                        {
+                            HasAdded = true;
+                        }
+                        nestLV++;
+                    }
+                }
+                if (strCondition.Substring(i, 1) == ")")
+                {
+                    if (IsIndexOn)
+                    {
+                        IsIndexOn = false;
+                    }
+                    else
+                    {
+                        nestLV--;
+                        if (nestLV == 1)
+                        {
+                            HasAdded = true;
+                        }
+                    }
+                }
+                foreach (var logicOpt in LogicOplst)
+                {
+                    if (i + logicOpt.Length <= strCondition.Length && strCondition.Substring(i, logicOpt.Length) == logicOpt)
+                    {
+                        if (nestLV == 1)
+                        {
+                            if (!String.IsNullOrEmpty(subCondition))
+                            {
+                                TopConditionLst.Add(subCondition);
+                            }
+                            TopConditionLst.Add(logicOpt);
+                            subCondition = String.Empty;
+                            HasAdded = true;
+                            i += logicOpt.Length - 1;
+                        }
+                    }
+                }
+                if (!HasAdded)
+                {
+                    subCondition += strCondition.Substring(i, 1);
+                }
+            }
+            if (!string.IsNullOrEmpty(subCondition))
+            {
+                TopConditionLst.Add(subCondition);
+                subCondition = String.Empty;
+            }
+            foreach (var ConditionString in TopConditionLst)
+            {
+                Boolean isCondition = true;
+                foreach (var logicOpt in LogicOplst)
+                {
+                    if (ConditionString == logicOpt)
+                    {
+                        isCondition = false;
+                    }
+                }
+                if (isCondition) GetTestItem(ConditionString);
+            }
+            if (TestItemLst.Count == 0)
+            {
+                TestItemLst.Add(OrgCondition.Trim());
+            }
+        }
+        private void GetTestItem(String ConditionString)
+        {
+            Boolean PickerStatus = true;
+            String TestItem = String.Empty;
+            for (int i = 0; i < ConditionString.Length; i++)
+            {
+                //(Ｗ＿項目番号 = 070 OR 075) AND 端末機種別コード OF ＬＣ制御共通情報 = ★郵便局用端末機
+                foreach (var MathsOpt in MathOplst)
+                {
+                    if (i + MathsOpt.Length <= ConditionString.Length && ConditionString.Substring(i, MathsOpt.Length) == MathsOpt)
+                    {
+                        if (IsConst(TestItem))
+                        {
+                            //如果大小比较的左侧是常数，则获得右边的东西 10 = A
+                            PickerStatus = true;
+                        }
+                        else
+                        {
+                            //正常的关系式 A = 10
+                            PickerStatus = false;
+                            TestItemLst.Add(TestItem.Trim());
+                        }
+                        TestItem = String.Empty;
+                        i += MathsOpt.Length - 1;
+                    }
+                }
+
+                //(Ｗ＿項目番号 = 070 OR 075) 
+                foreach (var LogicOpt in LogicOplst)
+                {
+                    if (i + LogicOpt.Length <= ConditionString.Length && ConditionString.Substring(i, LogicOpt.Length) == LogicOpt)
+                    {
+                        if (PickerStatus == true)
+                        {
+                            // 遇见了OR的时候
+                            // And|Or 前面如果是 10 = A 的补偿，则需要补偿
+                            if (!IsConst(TestItem))
+                            {
+                                TestItemLst.Add(TestItem.Trim());
+                            }
+                            TestItem = String.Empty;
+                        }
+                        PickerStatus = true;
+                        i += LogicOpt.Length - 1;
+                    }
+                }
+                if (PickerStatus)
+                {
+                    if (ConditionString.Substring(i, 1) != " ")
+                    {
+                        if (!(ConditionString.Substring(i, 1) == "(" && TestItem.Length == 0))
+                        {
+                            TestItem = TestItem + ConditionString.Substring(i, 1);
+                        }
+                    }
+                    else
+                    {
+                        if (i + " OF ".Length <= ConditionString.Length && ConditionString.Substring(i, 4) == " OF ")
+                        {
+                            TestItem = TestItem + " OF ";
+                            i += " OF ".Length - 1;
+                        }
+                    }
+                }
+            }
+            //如果 10=A 出现在最后的是，需要补偿
+            if (PickerStatus == true)
+            {
+                // 10 = A 的补偿
+                if (!IsConst(TestItem)) TestItemLst.Add(TestItem.Trim());
+                TestItem = String.Empty;
+            }
+
+        }
+        /// <summary>
+        /// 是否为常数
+        /// </summary>
+        /// <param name="TestString"></param>
+        /// <returns></returns>
+        private bool IsConst(String TestString)
+        {
+            TestString = TestString.Trim();
+            Boolean R;
+            R = false;
+            int t;
+            if (int.TryParse(TestString, out t))
+            {
+                R = true;
+            }
+            //常数不用加入
+            if (TestString.StartsWith("☆") || TestString.StartsWith("★"))
+            {
+                R = true;
+            }
+            if (TestString.StartsWith("LOW-VALUE") || TestString.StartsWith("HIGH-VALUE"))
+            {
+                R = true;
+            }
+            if (TestString.Equals("SPACE") || TestString.Equals("ZERO"))
+            {
+                R = true;
+            }
+            //引号
+            if (TestString.StartsWith("\"") && TestString.EndsWith("\""))
+            {
+                R = true;
+            }
+            //NC汉字
+            if (TestString.StartsWith("NC\"") && TestString.EndsWith("\""))
+            {
+                R = true;
+            }
+            return R;
+        }
+        /// <summary>
+        /// 临界值测试
+        /// </summary>
+        /// <param name="Conition"></param>
+        /// <returns></returns>
+        private string GetBoundTestCondition(string Condition)
+        {
+            ///条件文
+            String OpCondition = String.Empty;
+
+            return OpCondition;
+        }
+        /// <summary>
+        /// 获得当前条件的反条件
+        /// </summary>
+        /// <param name="Condition"></param>
+        /// <returns></returns>
+        private string GetOpsiteCondition(string Condition)
+        {
+            //TEST
+            //GetConditionFormat(Condition);
+            if (String.IsNullOrEmpty(Condition))
+            {
+                return string.Empty;
+            }
+            /// A = B => A <> B
+            /// A <> B => A = B
+            /// 暂时不考虑 AND OR
+            const String EqualString = " = ";
+            const String NotEqualString = " ^= ";
+            String OpCondition = String.Empty;
+            if (Condition.Contains(EqualString))
+            {
+                String[] SplitString = new string[1] { EqualString };
+                OpCondition = Condition.Split(SplitString, 10, StringSplitOptions.RemoveEmptyEntries)[0] + NotEqualString + Condition.Split(SplitString, 10, StringSplitOptions.RemoveEmptyEntries)[1];
+            }
+            else
+            {
+                if (Condition.Contains(NotEqualString))
+                {
+                    String[] SplitString = new string[1] { NotEqualString };
+                    OpCondition = Condition.Split(SplitString, 10, StringSplitOptions.RemoveEmptyEntries)[0] + EqualString + Condition.Split(SplitString, 10, StringSplitOptions.RemoveEmptyEntries)[1];
+                }
+            }
+            return OpCondition;
+        }
+    }
+}
