bbef484a1cf7f9b957f1706dc25b94432e3f9535
MagicHu <huweimin@cn.ibm.com>
2014-05-10 21:40:52 +0800

    通信初步

--- Card/Server/Communication.cs
+++ Card/Server/Communication.cs
@@ -1,7 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 
 namespace Card.Server
 {
@@ -10,6 +12,79 @@ namespace Card.Server
     /// </summary>
     public static class Communication
     {
+        /// <summary>
+        /// 开启服务器
+        /// </summary>
+        public static void StartServer()
+        {
+            TcpListener server = null;
+            try
+            {
+                Int32 port = 13000;
+                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+                server = new TcpListener(localAddr, port);
+                server.Start();
+                while (true)
+                {
+                    //对于每个请求创建一个线程，线程的参数是TcpClient对象
+                    TcpClient client = server.AcceptTcpClient();
+                    ParameterizedThreadStart ParStart = ProcessRequest;
+                    var t = new Thread(ParStart);
+                    t.Start(client);
+                }
+            }
+            catch (SocketException)
+            {
+
+            }
+            finally
+            {
+                // Stop listening for new clients.
+                server.Stop();
+                server = null;
+            }
+        }
+        /// <summary>
+        ///     进行一次请求处理操作
+        /// </summary>
+        /// <param name="clientObj"></param>
+        private static void ProcessRequest(object clientObj)
+        {
+            var client = clientObj as TcpClient;
+            // Buffer for reading data
+            var bytes = new Byte[512];
+            NetworkStream stream = client.GetStream();
+            ///实际长度
+            int ActualSize;
+            ///
+            String Request = String.Empty;
+            //512Byte单位进行处理
+            while ((client.Available != 0) && (ActualSize = stream.Read(bytes, 0, bytes.Length)) != 0)
+            {
+                Request = Encoding.ASCII.GetString(bytes, 0, ActualSize);
+            }
+            bytes = Encoding.ASCII.GetBytes("Response Of [" + Request + "]");
+            stream.Write(bytes, 0, bytes.Length);
+            client.Close();
+        }
+        public static String Request(String Request) {
+            TcpClient client = new TcpClient();
+            client.Connect("localhost", 13000);
+            var stream = client.GetStream();
+            var bytes = new Byte[512];
+            bytes = Encoding.ASCII.GetBytes(Request);
+            stream.Write(bytes, 0, bytes.Length);
+            String Response = String.Empty;
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                while (reader.Peek() != -1)
+                {
+                    Response = reader.ReadLine();
+                }
+            }
+            client.Close();
+            return Response;
+        }
         /// <summary>
         /// 作为消息的前3位传输
         /// </summary>
--- 炉边传说/frmStartGame.cs
+++ 炉边传说/frmStartGame.cs
@@ -1,13 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
-using System.Net;
-using System.Net.Sockets;
 
 namespace 炉边传说
 {
@@ -18,19 +10,13 @@ namespace 炉边传说
             InitializeComponent();
         }
         /// <summary>
-        /// 
+        /// 开始游戏的请求
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void btnCreateGame_Click(object sender, EventArgs e)
         {
-            TcpClient client = new TcpClient();
-            client.Connect("localhost", 13000);
-            var stream = client.GetStream();
-            var bytes = new Byte[512];
-            bytes = Encoding.ASCII.GetBytes("STARTGAME");
-            stream.Write(bytes, 0, bytes.Length);
-            client.Close();
+            System.Diagnostics.Debug.WriteLine(Card.Server.Communication.Request("STARTGAME"));
         }
     }
 }
