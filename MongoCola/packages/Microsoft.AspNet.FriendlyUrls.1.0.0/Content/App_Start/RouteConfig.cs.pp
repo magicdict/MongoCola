9fc8de855304c1e0222444c25a692ae81c018696
MagicHu <mynightelfplayer@hotmail.com>
2015-01-13 15:22:18 +0800

    CleanUp

---
+++ MongoCola/packages/Microsoft.AspNet.FriendlyUrls.1.0.0/Content/App_Start/RouteConfig.cs.pp
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Routing;
+using Microsoft.AspNet.FriendlyUrls;
+
+namespace $rootnamespace$
+{
+    public static class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.EnableFriendlyUrls();
+        }
+    }
+}
